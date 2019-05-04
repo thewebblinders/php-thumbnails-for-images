9511eb2433a6cd2e1ca1d5b2fb83890c0e528a94
thewebblinders <thewebblinders@gmail.com>
2019-05-05 02:18:52 +0530

    PHP - How to generate thumbnails for images (png, jpg, gif, webp) with out Imagick

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+use claviska\SimpleImage;
+
+include './claviska-SimpleImage/SimpleImage.php';
+
+class ThumbNailer extends SimpleImage{
+    /**
+     * Generates thumbnail for PNG,JPG,GIF,WEBP FORMATS
+     * Returns true if thumbnail is created
+     *
+     * @param string $source - path to the source file  
+     * @param string $destination - destination path along with file name
+     * @param integer $width - width of the thumbnail image
+     * @param integer $height - height of the thumbnail image
+     * @return bool
+     */
+    public function generateThumbnail($source,$destination,$width=300,$height=300){
+        try{
+            $this->fromFile($source)
+            ->thumbnail($width, $height, "center")
+            ->toFile($destination); 
+            return true; 
+        }
+        catch(Exception $e){
+            return false;
+        }
+ 
+    }
+}
+$tn=new ThumbNailer();
+$tn->generateThumbnail('./source-images/A-PNG-IMAGE.png','./thumbnail-images/png.png');
+$tn->generateThumbnail('./source-images/A-JPG-IMAGE.jpg','./thumbnail-images/jpg.jpg');
+$tn->generateThumbnail('./source-images/A-GIF-IMAGE.gif','./thumbnail-images/gif.gif');
+
+
+
